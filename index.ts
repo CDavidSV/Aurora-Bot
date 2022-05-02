108ca02cad5e8d7fe4ac220610c5f3af7b3ba412
CDavidSV <A00834448@itesm.mx>
2022-05-01 22:10:27 -0500

    First Test
    Created Bot, basic command

---
+++ index.ts
@@ -0,0 +1,29 @@
+import DiscordJS, { Intents } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+let prefix = '%';
+
+// Create client and add intents.
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.on('ready', bot => {
+    console.log(`Logged in as ${bot.user.tag}`);
+});
+
+// Normal commands with prefix.
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+
+})
+
+// Token.
+client.login(process.env.TOKEN);
