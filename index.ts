10b1587d16335358ebb545133caac49c4ae962f0
CDavidSV <A00834448@itesm.mx>
2022-05-01 23:12:28 -0500

    modification to commands handler

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import DiscordJS, { Intents } from 'discord.js';
+import DiscordJS, { Intents, MessageEmbed } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 let prefix = '%';
@@ -12,17 +12,28 @@ const client = new DiscordJS.Client({
 });
 
 client.on('ready', bot => {
-    console.log(`Logged in succesfully as ${bot.user.tag}`);
+    console.log(`Succesfully logged in  as ${bot.user.tag}`);
 });
 
 // Normal commands with prefix.
 client.on('messageCreate', message => {
-    if (message.author.bot) return;
+    // Verify that the message author is not the bot and that it has the correct prefix
+    if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-    if (message.content === 'ping') {
-        message.reply('pong');
+    // Rmoves prefix and converts the message into lowercase.
+    const command = message.content.slice(prefix.length).toLowerCase();
+
+    // Commands.
+    switch (command) {
+        case 'ping':
+            message.reply('pong');
+            break;
+        default:
+            const noCommandEmbed = new MessageEmbed()
+                .setColor('#FF0000')
+                .setDescription('Error: No Such Command')
+            message.reply({ embeds: [noCommandEmbed] });
     }
-
 })
 
 // Token.
