83bef3a555b3b5cc8fc5595f265b790b86789714
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-05-10 15:48:35 -0600

    Update ready.ts

--- events/ready.ts
+++ events/ready.ts
@@ -6,9 +6,9 @@ export default {
     name: Events.ClientReady,
     once: true,
     async execute(client: Client) {
-        //await mongoose.connect(process.env.MONGO_URI!).then(() => { // Connect to mongo, url needs to be provided in a .env file.
-        //    console.log('Connected to mongo'.green);
-        //});
+        await mongoose.connect(process.env.MONGO_URI!).then(() => { // Connect to mongo, url needs to be provided in a .env file.
+            console.log('Connected to mongo'.green);
+        });
         console.log(`Logged in as ${client.user!.tag}`.green);
 
         // On bot ready.
