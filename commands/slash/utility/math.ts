ec3409daafe7a3e799a87c5fe5188aa4ff67724a
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-05-11 01:00:22 -0600

    Added math command

---
+++ commands/slash/utility/math.ts
@@ -0,0 +1,33 @@
+import { CacheType, ChatInputCommandInteraction, ColorResolvable, EmbedBuilder } from "discord.js";
+import { evaluate } from "mathjs";
+import config from "../../../config.json";
+
+export default {
+    subCommand: "utility.math",
+    callback: (interaction: ChatInputCommandInteraction<CacheType>) => {
+        const expression = interaction.options.getString("expression")!;
+        const mathRegex = /^(?:[\d]+[.][\d]+|[\d]+|\+|\-|\*|\/|\^|\(|\)|sin+|cos+|tan+|E+|LN10+|e+|ln10+|ln2+|LN2+|LOG2E+|log2e+|pi+|PI+|SQRT1_2+|sqrt1_2+|SQRT2+|sqrt2+|abs+|acos+|acosh+|asin+|asinh+|atan+|atan2+|atanh+|cbrt+|ceil+|clz32+|cosh+|exp+|expm1+|floor+|fround+|hypot+|imul+|log+|log10+|log1p+|log2+|max+|min+|pow+|random+|round+|sign+|sinh+|sqrt+|tanh+|trunc+|det+| )+$/g;
+
+        if (!mathRegex.test(expression)) {
+            interaction.reply({content: `Incorrect math expression for: **${expression}**`});
+            return;
+        }  
+
+        try {
+            const result = evaluate(expression);
+            const mathEmbed = new EmbedBuilder()
+                .setAuthor({ name: 'Aurora', iconURL: interaction.client!.user!.avatarURL()! })
+                .setColor(config.embeds.colors.main as ColorResolvable)
+                .addFields(
+                    { name: "Math Expression: ", value: "```css\n" + expression + " \n```"},
+                    { name: "Answer: ", value: "```css\n" + result.toString() + " \n```"}
+                )
+                .setFooter({ text: config.version })
+            interaction.reply({ embeds: [mathEmbed] });
+
+        } catch (err){
+            console.log(err);
+            interaction.reply({content: `Incorrect math expression for: **${expression}**`});
+        }
+    }
+}
--- commands/slash/utility/utility.ts
+++ commands/slash/utility/utility.ts
@@ -1,27 +1,31 @@
-// import { CacheType, ChatInputCommandInteraction, PermissionsBitField, SlashCommandBuilder } from "discord.js";
-// import config from "../../config.json";
+import { PermissionsBitField, SlashCommandBuilder } from "discord.js";
 
-// export default {
-//     data: new SlashCommandBuilder()
-//         .setName('utility')
-//         .setDescription('🔨 Set of usefull commands')
-//         .addSubcommand(subcommand =>
-//             subcommand
-//                 .setName('math')
-//                 .setDescription('🧮 Solve an exprestion based math problem'))
-//         .addSubcommand(subcommand =>
-//             subcommand
-//                 .setName('convert')
-//                 .setDescription('🔢 Convert a specified unit to another format'))
-//         .addSubcommand(subcommand =>
-//             subcommand
-//                 .setName('translate')
-//                 .setDescription('📝 Translate an image or text to any language'))
-//         .addSubcommand(subcommand =>
-//             subcommand
-//                 .setName('define')
-//                 .setDescription('📚 Search the definition of a word')),
-//     botPerms: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages],
-//     async execute(interaction: ChatInputCommandInteraction<CacheType>) {
-//     }
-// }
+export default {
+    data: new SlashCommandBuilder()
+        .setName('utility')
+        .setDescription('🔨 Set of usefull commands')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('math')
+                .setDescription('🧮 Solve an exprestion based math problem')
+                .addStringOption(option =>
+                    option
+                        .setName('expression')
+                        .setDescription('Enter any math expression here. e.g: (4 + 5) * 3')
+                        .setRequired(true)
+                ))
+        // .addSubcommand(subcommand =>
+        //     subcommand
+        //         .setName('convert')
+        //         .setDescription('🔢 Convert a specified unit to another format'))
+        // .addSubcommand(subcommand =>
+        //     subcommand
+        //         .setName('translate')
+        //         .setDescription('📝 Translate an image or text to any language'))
+        // .addSubcommand(subcommand =>
+        //     subcommand
+        //         .setName('define')
+        //         .setDescription('📚 Search the definition of a word'))
+        .setDMPermission(true),
+    botPerms: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages],
+}
